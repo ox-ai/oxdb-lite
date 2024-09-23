71e4a685adfe8cdae8a3d5517006e6e2304248ec
Lokeshwaran M <80915494+Lokeshwaran-M@users.noreply.github.com>
2024-09-23 15:37:00 +0530

    Update settings.py Dbin method defaults to "Oxdbin"

--- oxdb/settings.py
+++ oxdb/settings.py
@@ -4,6 +4,6 @@
 class settings:
     EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
     EMBEDDING_MODELS = ["sentence-transformers/all-MiniLM-L6-v2"]
-    DBIN_METHOD = "bson"
+    DBIN_METHOD = "oxdbin"
     SIM_FORMAT = "dp"
     SIM_FORMATS = ["dp", "ed", "cs"]
